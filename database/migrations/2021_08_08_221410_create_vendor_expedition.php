80e23f97805de36d40eab4e7c2b85d4823256c59
ahmad <ahmad.afandi85@gmail.com>
2021-08-24 05:27:58 +0700

    rename migration file

--- database/migrations/2021_08_08_221410_create_vendor_expedition.php
+++ database/migrations/2021_08_08_221410_create_vendor_expedition.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVendor extends Migration
+class CreateVendorExpedition extends Migration
 {
     /**
      * Run the migrations.
