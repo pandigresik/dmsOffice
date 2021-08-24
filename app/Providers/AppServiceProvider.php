5ea375cc45b33bd35b0512a09d92a3108ab11ee4
ahmad <ahmad.afandi85@gmail.com>
2021-08-24 21:17:09 +0700

    force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
         // config(['app.locale' => 'id']);
         // Carbon::setLocale('id');
         if (env('APP_DEBUG')) {
