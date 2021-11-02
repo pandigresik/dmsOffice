502029546729a1ee6c30d0642c439cc9980ad66a
ahmad <ahmad.afandi85@gmail.com>
2021-11-02 14:03:23 +0700

    perbaikan migrasi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionsTableSeeder::class);
         $this->call(MenusTableSeeder::class);                
         $this->call(RolesTableSeeder::class);
-        $this->call(SettingTableSeeder::class);
+        //$this->call(SettingTableSeeder::class);
         $this->call(ModelHasRolesTableSeeder::class);
         $this->call(RoleHasPermissionsTableSeeder::class);
         $this->call(MenuPermissionsTableSeeder::class);
