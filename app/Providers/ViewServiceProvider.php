53dcdf978e1d42efc2c669339c9ceb4fbdd61d2e
ahmad <ahmad.afandi85@gmail.com>
2022-04-05 21:38:44 +0700

    fix: error generate

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -19,11 +19,7 @@ class ViewServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot()
-    {
-        View::composer(['inventory.product_stock.fields'], function ($view) {
-            $productItems = Product::pluck('id')->toArray();
-            $view->with('productItems', $productItems);
-        });
+    {        
         // View::composer(['finance.debit_credit_note.fields'], function ($view) {
         //     $partnerItems = Partner::pluck('id')->toArray();
         //     $view->with('partnerItems', $partnerItems);
