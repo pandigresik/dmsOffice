e7d142ed832af50a4587cb4531be34638b09e01c
ahmad afandi <ahmad.afandi85@gmail.com>
2023-09-12 16:08:18 +0700

    tambahkan kolom morphing pada valuasi persediaan

--- resources/views/inventory/valuasi_inventory/show.blade.php
+++ resources/views/inventory/valuasi_inventory/show.blade.php
@@ -6,9 +6,13 @@
             <th rowspan="2">Sumber Data</th>
             <th colspan="3">Masuk</th>
             <th colspan="3">Keluar</th>
+            <th colspan="3">Morphing</th>
             <th colspan="3">Saldo</th>
         </tr>
         <tr>
+            <th>Qty</th>
+            <th>Harga</th>
+            <th>Total</th>
             <th>Qty</th>
             <th>Harga</th>
             <th>Total</th>
@@ -22,7 +26,7 @@
     </thead>
     <tbody>
         @php
-            $totalMasukQty = $totalMasukVal = $totalKeluarQty = $totalKeluarVal = 0;
+            $totalMasukQty = $totalMasukVal = $totalKeluarQty = $totalKeluarVal = $totalMorphingQty = $totalMorphingVal = 0;
             $saldoAwalQty = 0;
             $saldoAwalVal = 0;
             $saldoAwalPrice = 0;
@@ -49,20 +53,28 @@
             <td></td>
             <td></td>
             <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td class="text-right">{{ $exportExcel ? $saldoAwalVal : localNumberFormat($saldoAwalVal, 0)}}</td>
         </tr>
+        
         @foreach ($detail as $item)
             @php
                 $masukQty = in_array($item->szTrnId, ['DMSDocStockInBranch','DMSDocStockInDistribution','DMSDocStockInSupplier']) ? $item->decQtyOnHand : 0;
                 $keluarQty = in_array($item->szTrnId, ['DMSDocStockOutBranch','DMSDocStockOutDistribution','DMSDocStockOutSupplier']) ? $item->decQtyOnHand : 0;
+                $morphingQty = in_array($item->szTrnId, ['DMSDocStockMorph']) ? $item->decQtyOnHand : 0;                
                 $masukVal = $masukQty * $item->price;
                 $keluarVal = $keluarQty * $item->price;
+                $morphingVal = $morphingQty * $item->price;
                 $totalMasukQty += $masukQty; 
                 $totalMasukVal += $masukQty * $item->price;
                 $totalKeluarQty += $keluarQty;
                 $totalKeluarVal += $keluarQty * $item->price;
-                $saldoAkhirQty += ($masukQty + $keluarQty);
-                $saldoAkhirVal += ($masukVal + $keluarVal);
+                $totalMorphingQty +=  $morphingQty;
+                $totalMorphingVal +=  $morphingVal;
+                $saldoAkhirQty += ($masukQty + $keluarQty + $morphingQty);
+                $saldoAkhirVal += ($masukVal + $keluarVal + $morphingVal);
             @endphp            
             <tr>
                 <td>{{ localFormatDate($item->dtmTransaction) }}</td>
@@ -74,6 +86,9 @@
                 <td class="text-right">{{ $exportExcel ? $keluarQty : localNumberFormat($keluarQty,0) }}</td>
                 <td class="text-right">{{ $exportExcel ? $item->price : localNumberFormat($item->price,0) }}</td>
                 <td class="text-right">{{ $exportExcel ? ($keluarQty * $item->price) : localNumberFormat($keluarQty * $item->price,0) }}</td>
+                <td class="text-right">{{ $exportExcel ? $morphingQty : localNumberFormat($morphingQty,0) }}</td>
+                <td class="text-right">{{ $exportExcel ? $item->price : localNumberFormat($item->price,0) }}</td>
+                <td class="text-right">{{ $exportExcel ? ($morphingQty * $item->price) : localNumberFormat($morphingQty * $item->price,0) }}</td>
                 <td class="text-right">{{ $exportExcel ? $saldoAkhirQty : localNumberFormat($saldoAkhirQty,0) }}</td>
                 <td class="text-right">{{ $exportExcel ? $item->price : localNumberFormat($item->price,0) }}</td>
                 <td class="text-right">{{ $exportExcel ? $saldoAkhirVal : localNumberFormat($saldoAkhirVal,0) }}</td>
@@ -89,6 +104,9 @@
             <th class="text-right">{{ $exportExcel ? $totalKeluarQty : localNumberFormat($totalKeluarQty,0) }}</th>
             <th></th>
             <th class="text-right">{{ $exportExcel ? $totalKeluarVal : localNumberFormat($totalKeluarVal,0) }}</th>
+            <th class="text-right">{{ $exportExcel ? $saldoAkhirQty : localNumberFormat($saldoAkhirQty,0) }}</th>
+            <th></th>
+            <th class="text-right">{{ $exportExcel ? $totalMorphingVal : localNumberFormat($totalMorphingVal,0) }}</th>
             <th class="text-right">{{ $exportExcel ? $saldoAkhirQty : localNumberFormat($saldoAkhirQty,0) }}</th>
             <th></th>
             <th class="text-right">{{ $exportExcel ? $saldoAkhirVal : localNumberFormat($saldoAkhirVal,0) }}</th>
